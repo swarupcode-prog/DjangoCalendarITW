43e7956a4c79c5d57558ffc4ab58b2db8efa54d8
vamsikunal <75450629+vamsikunal@users.noreply.github.com>
2021-06-28 14:27:03 +0530

    Update admin.py

--- FinalCal/app/admin.py
+++ FinalCal/app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import UserProfileInfo, Event
+from .models import Event
 # Register your models here.
 
-admin.site.register(UserProfileInfo)
 admin.site.register(Event)
