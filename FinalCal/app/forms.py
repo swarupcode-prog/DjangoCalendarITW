9d2631cac1e14ec9496c0103cec1bb79676b0b13
vamsikunal <75450629+vamsikunal@users.noreply.github.com>
2021-06-28 12:58:42 +0530

    Update forms.py

--- FinalCal/app/forms.py
+++ FinalCal/app/forms.py
@@ -2,23 +2,29 @@ from django import forms
 from .models import UserProfileInfo, Event
 from django.contrib.auth.models import User
 
+# Model form of model 'User' inbuilt in Django
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.EmailField(required=True, label='Email',
                     widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     username = forms.CharField(max_length=20, label='Username',
                     widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    
+    # Django Meta class used to transfer information about the model to the Django forms
     class Meta:
         model = User
-        fields = ('username','email','password')
+        fields = ('username','email','password') # The generated Form class will have a form field for every model field specified, in the order specified in the fields attribute.
 
-class UserProfileForm(forms.ModelForm):
-    
+# Model form of model 'UserProfileInfo' in models.py
+class UserProfileForm(forms.ModelForm):  
+    # Django Meta class used to transfer information about the model to the Django forms
     class Meta:
         model = UserProfileInfo
         fields = ('gender',)
 
+# Model form of model 'Event' in models.py
 class EventForm(forms.ModelForm):
+    # Django Meta class used to transfer information about the model to the Django forms
   class Meta:
     model = Event
     # datetime-local is a HTML5 input type, format to make date time show on fields
@@ -26,7 +32,7 @@ class EventForm(forms.ModelForm):
       'start_time': forms.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
       'end_time': forms.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
     }
-    exclude = ['user']
+    exclude = ['user'] # The exclude attribute tells Django what fields from the model not to include in the form.
 
   def __init__(self, *args, **kwargs):
     super(EventForm, self).__init__(*args, **kwargs)
